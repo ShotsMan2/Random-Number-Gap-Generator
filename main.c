12ee5cedafefbf0e26342bfe891ceb2a4597ffc6
Selim Efe <uysalselimefe@gmail.com>
2026-01-13 17:45:46 +0300

    Implement minimum gap constraint logic for random generation
    Added source code that generates a sequence of random numbers in the range [10, 70]. The algorithm implements a validation loop to ensure the absolute difference between the new candidate and all existing numbers is greater than 5.

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+     int i,sayi,n,uygunluk,j,fark,deneme;
+
+    printf("n i giriniz");
+    scanf("%d",&n);
+
+    int dizi[99];
+
+    srand(time(NULL));
+
+    for(i=0;i<n;i++){
+        for(;;){
+            uygunluk=1;
+
+            sayi=rand()%61+10;
+
+            for(j=0;j<i;j++){
+
+
+
+                if(dizi[j]>sayi){
+                    fark=dizi[j]-sayi;
+                }
+                else{
+                    fark=sayi-dizi[j];
+                }
+
+                if(fark<=5){
+                    uygunluk=0;
+                    break;
+                }
+
+            }
+            if(uygunluk==1){
+                break;
+            }
+
+            deneme++;
+
+            if(deneme>1000){
+                printf("\n dolud");
+
+                return 0;
+            }
+        }
+        dizi[i]=sayi;
+        printf("%d ",dizi[i]);
+    }
+}
